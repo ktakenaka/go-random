3875273c88c3264ccfb7042fe4dfcd3d2cf51431
Takenaka Kazumasa <bzzs.1120@gmail.com>
2020-07-19 14:13:30 +0700

    define CRUD method for sample entity. TODO: refactor to use clean architecture

--- app/interface/api/handler/sample_handler.go
+++ app/interface/api/handler/sample_handler.go
@@ -1,18 +1,85 @@
 package handler
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/ktakenaka/go-random/app/domain/entity"
 )
 
-func getSmaple(ctx *gin.Context) {
+func getSamples(ctx *gin.Context) {
+	samples, err := entity.ListSamples()
+	if err != nil {
+		log.Print(err)
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "NG"})
+	} else {
+		ctx.JSON(http.StatusOK, gin.H{"samples": samples})
+	}
+}
+
+func getSample(ctx *gin.Context) {
 	id := ctx.Params.ByName("id")
-	fmt.Println(id)
-	ctx.JSON(http.StatusOK, gin.H{"message": "OK"})
+	sample, err := entity.FindSample(id)
+
+	if err != nil {
+		log.Print(err)
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "NG"})
+	} else {
+		ctx.JSON(http.StatusOK, gin.H{"sample": sample})
+	}
+}
+
+func postSample(ctx *gin.Context) {
+	title := ctx.PostForm("title")
+	err := entity.CreateSample(title)
+	if err != nil {
+		log.Print(err)
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "NG"})
+	} else {
+		ctx.JSON(http.StatusOK, gin.H{"message": "OK"})
+	}
+}
+
+func putSample(ctx *gin.Context) {
+	sample, err := entity.FindSample(ctx.Params.ByName("id"))
+	if err != nil {
+		log.Print(err)
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "NG"})
+	}
+
+	sample.Title = ctx.PostForm("title")
+	err = sample.Save()
+
+	if err != nil {
+		log.Print(err)
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "NG"})
+	} else {
+		ctx.JSON(http.StatusOK, gin.H{"message": "OK"})
+	}
+}
+
+func deleteSample(ctx *gin.Context) {
+	sample, err := entity.FindSample(ctx.Params.ByName("id"))
+	if err != nil {
+		log.Print(err)
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "NG"})
+	}
+
+	err = sample.Delete()
+
+	if err != nil {
+		log.Print(err)
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "NG"})
+	} else {
+		ctx.JSON(http.StatusOK, gin.H{"message": "OK"})
+	}
 }
 
 func AddSampleHanlder(g *gin.RouterGroup) {
-	g.GET("/:id", getSmaple)
+	g.GET("", getSamples)
+	g.GET("/:id", getSample)
+	g.POST("", postSample)
+	g.PUT("/:id", putSample)
+	g.DELETE("/:id", deleteSample)
 }
