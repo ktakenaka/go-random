3db4b6917b4d7d3a880c68bdc3cdc8483cb01bc6
Takenaka Kazumasa <bzzs.1120@gmail.com>
2020-07-25 14:29:04 +0700

    organize codes following clean architecture

--- app/domain/entity/sample.go
+++ app/domain/entity/sample.go
@@ -1,62 +1,31 @@
 package entity
 
 import (
-	"errors"
 	"time"
-
-	"github.com/ktakenaka/go-random/app/interface/persistence/mysql"
 )
 
 type Sample struct {
-	ID        uint `gorm:"primary_key"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	Title     string
+	ID        int       `gorm:"primary_key" json:"id"`
+	Title     string    `json:"title"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 func (s *Sample) TableName() string {
 	return "samples"
 }
 
-// TODO: consider the place to put, including db.Close()
-var db = mysql.DBConnection()
-
-func CreateSample(title string) error {
+func NewSample(id int, title string) *Sample {
 	now := time.Now()
-
-	sample := Sample{
-		Title:     title,
-		CreatedAt: now,
-		UpdatedAt: now,
+	return &Sample{
+		Title: title, CreatedAt: now, UpdatedAt: now,
 	}
-
-	db.Create(&sample)
-	if db.NewRecord(sample) {
-		return errors.New("failed to create Record")
-	}
-	return nil
 }
 
-func ListSamples() ([]Sample, error) {
-	var samples []Sample
-	err := db.Find(&samples).Error
-
-	return samples, err
+func (s *Sample) GetID() int {
+	return s.ID
 }
 
-func FindSample(id string) (Sample, error) {
-	var sample Sample
-	err := db.First(&sample, id).Error
-
-	return sample, err
-}
-
-func (s Sample) Save() error {
-	err := db.Save(&s).Error
-	return err
-}
-
-func (s Sample) Delete() error {
-	err := db.Delete(&s).Error
-	return err
+func (s *Sample) GetTitle() string {
+	return s.Title
 }
