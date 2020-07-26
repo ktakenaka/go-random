4d366cac827c5990d2d0e3bc298d3d340fa0c9ea
Takenaka Kazumasa <bzzs.1120@gmail.com>
2020-07-26 15:43:32 +0700

    use wire to build DI initialization automatically

---
+++ app/registry/usecase.go
@@ -0,0 +1,35 @@
+// +build wireinject
+// The build tag makes sure the stub is not built in the final build.
+
+package registry
+
+import (
+	"github.com/google/wire"
+	"github.com/ktakenaka/go-random/app/domain/service"
+	"github.com/ktakenaka/go-random/app/domain/repository"
+	"github.com/ktakenaka/go-random/app/external/database"
+	"github.com/ktakenaka/go-random/app/interface/persistence/mysql"
+	"github.com/ktakenaka/go-random/app/usecase"
+)
+
+func InitializeSampleUsecase() *usecase.SampleUsecase {
+	wire.Build(SampleUsecaseSet)
+	return &usecase.SampleUsecase{}
+}
+
+var (
+	SampleUsecaseSet = wire.NewSet(
+		database.MySQLConnection,
+		sampleRepositorySet,
+		service.NewSampleService,
+		usecase.NewSampleUsecase,
+	)
+)
+
+var (
+	sampleRepositorySet = wire.NewSet(
+		mysql.NewSampleRepository,
+		wire.Bind(new(repository.SampleRepository), new(*mysql.SampleRepository)),
+
+	)
+)
