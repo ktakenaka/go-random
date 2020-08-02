c14ccbc6922a9b0b5b8ea15277262a79ac08f477
Takenaka Kazumasa <bzzs.1120@gmail.com>
2020-08-02 23:53:53 +0700

    make cli app

--- cmd/srv/main.go
+++ cmd/srv/main.go
@@ -2,19 +2,33 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/ktakenaka/go-random/app/config"
 	"github.com/ktakenaka/go-random/app/external/database"
 	"github.com/ktakenaka/go-random/app/external/framework"
+
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	if err := database.InitMySQLConnection(config.GenDBAceessStr()); err != nil {
-		log.Fatal(err)
+	app := &cli.App{
+		Name:  "go-random",
+		Usage: "try everything",
+		Action: func(c *cli.Context) error {
+			if err := database.InitMySQLConnection(config.GenDBAceessStr()); err != nil {
+				return err
+			}
+
+			router := framework.Handler()
+			if err := router.Run(":8080"); err != nil {
+				return err
+			}
+			return nil
+		},
 	}
 
-	router := framework.Handler()
-	if err := router.Run(":8080"); err != nil {
+	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
 }
