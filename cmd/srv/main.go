05f57ddd24da1a5572475fba59dbb0ceeb84aa91
Takenaka Kazumasa <bzzs.1120@gmail.com>
2020-08-03 22:04:39 +0700

    enable flags

--- cmd/srv/main.go
+++ cmd/srv/main.go
@@ -15,6 +15,24 @@ func main() {
 	app := &cli.App{
 		Name:  "go-random",
 		Usage: "try everything",
+		Flags: []cli.Flag {
+			&cli.StringFlag{
+				Name: "mysqluser",
+				EnvVars: []string{"MYSQL_USER"},
+			},
+			&cli.StringFlag {
+				Name: "mysqlpassword",
+				EnvVars: []string{"MYSQL_PASSWORD"},
+			},
+			&cli.StringFlag {
+				Name: "dbhost",
+				EnvVars: []string{"DBHOST"},
+			},
+			&cli.StringFlag {
+				Name: "dbport",
+				EnvVars: []string{"DBPORT"},
+			},
+		},
 		Action: func(c *cli.Context) error {
 			if err := database.InitMySQLConnection(config.GenDBAceessStr()); err != nil {
 				return err
