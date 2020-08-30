1adef244beed4522caf237f886cd0e55cbab1b14
ktakenaka <bzzs.1120@gmail.com>
2020-08-31 01:16:16 +0700

    enable to export atoms

--- web/src/components/index.ts
+++ web/src/components/index.ts
@@ -1,3 +1,7 @@
+// Atoms
+export { default as Button } from './atoms/Button';
+export { default as Input } from './atoms/Input';
+
 // Organisms
 export { default as MainHeader } from './organisms/MainHeader';
 
