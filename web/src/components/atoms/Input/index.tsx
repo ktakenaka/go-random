0a64551524e42952fa50d64562925071f7a04514
Takenaka Kazumasa <bzzs.1120@gmail.com>
2020-08-31 20:20:42 +0700

    write all in Home temporarily

--- web/src/components/atoms/Input/index.tsx
+++ web/src/components/atoms/Input/index.tsx
@@ -7,24 +7,15 @@ type Props = {
 }
 
 const Input = ({ name }: Props) => {
-  const { values, handleChange, handleBlur, touched, errors } = useFormikContext();
-
-  let isInvalid = null;
-
-  if (touched[name] && errors[name]) {
-    isInvalid = true;
-  }
+  const { values, handleChange } = useFormikContext();
 
   return (
     <InputWrapper>
       <input
         value={getIn(values, name)}
         name={name}
-        className={isInvalid && `error`}
         onChange={handleChange}
-        onBlur={handleBlur}
       />
-      {isInvalid && <ErrorMessage>{errors[name]}</ErrorMessage>}
     </InputWrapper>
   )
 }
--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { MainTemplate } from '../../components';
 
-const HomePage = () => {
+const HomePage: React.FC = () => {
+  const [samples, setSamples] = useState<string[]>(['sample1', 'sample2', 'sample3']);
+  const [value, setvalue] = useState<string>('');
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>):void => {
+    const value = e.target.value;
+    setvalue(value);
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    setSamples([...samples.concat(value)]);
+  }
+  
   return (
     <MainTemplate>
       <h2>HOME</h2>
+
+      {samples.map((sample, index) => (
+        <li key={index}>{sample}</li>
+      ))}
+
+      <form onSubmit={onSubmit}>
+        <input type='text' onChange={onChange}/>
+        <input type='submit' value='submit' />
+      </form>
     </MainTemplate>
   );
 }
