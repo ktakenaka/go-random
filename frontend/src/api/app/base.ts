fd9fee4b92cd794a948f207f22d2b1f0abc23b32
Takenaka Kazumasa <bzzs.1120@gmail.com>
2020-09-14 20:05:53 +0700

    fe: add credentials to call be api

--- frontend/src/api/app/base.ts
+++ frontend/src/api/app/base.ts
@@ -1,5 +1,44 @@
 import axios from "axios";
+import { CSRF_TOKEN_KEY } from "constants/auth";
+import { UNAUTHORIZED } from "constants/metaCode";
+import history from "browserHistory";
 
-export default axios.create({
-  baseURL: `${process.env.REACT_APP_API_BASE_URL}/api/v1`,
-});
+const csrfIgnoreMethods = ["get", "head", "options"];
+
+const isAuthError = (response: any): boolean => {
+  return response.status === 401 && response.data.meta.code === UNAUTHORIZED;
+};
+
+const initialize = function (): void {
+  axios.defaults.withCredentials = true;
+  axios.defaults.baseURL = `${process.env.REACT_APP_API_BASE_URL}/api/v1`;
+
+  axios.interceptors.request.use(
+    function (config) {
+      if (config.method && !csrfIgnoreMethods.includes(config.method)) {
+        config.headers["X-CSRF-Token"] = localStorage.getItem(CSRF_TOKEN_KEY);
+      }
+      return config;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
+
+  axios.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    function (error) {
+      if (isAuthError(error.response)) {
+        // TOOD: try to refresh JWT before sign in again
+        history.push("/google/sign-in");
+      }
+      return Promise.reject(error);
+    }
+  );
+};
+
+initialize();
+
+export default axios;
---
+++ frontend/src/constants/metaCode.ts
@@ -0,0 +1,1 @@
+export const UNAUTHORIZED = 40101;
