ef0fbbdafbb59f57ac34a9d439797d743159d72f
Takenaka Kazumasa <bzzs.1120@gmail.com>
2020-09-15 00:42:22 +0700

    fix Sample to work with current table

--- backend/app/domain/entity/sample.go
+++ backend/app/domain/entity/sample.go
@@ -3,12 +3,15 @@ package entity
 import (
 	"time"
 
-	"github.com/go-playground/validator/v10"
+	validator "github.com/go-playground/validator/v10"
 )
 
 type Sample struct {
 	ID        int    `gorm:"primary_key"`
-	Title     string `validate:"required"`
+	Title     string `validate:"max=20, required"`
+	Content   string `validate:"max=100"`
+	UserID    uint64 `validate:required`
+	User      User
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
@@ -16,15 +19,6 @@ type Sample struct {
 func (s *Sample) TableName() string {
 	return "samples"
 }
-
-func NewSample(id int, title string) *Sample {
-	now := time.Now()
-	return &Sample{
-		Title: title, CreatedAt: now, UpdatedAt: now,
-	}
-}
-
-var validate *validator.Validate
 
 func (s *Sample) Validate() error {
 	validate = validator.New()
