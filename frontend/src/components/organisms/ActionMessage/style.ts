4f5541c8a6faf4b0dd84cf216ade76322a2d5d57
Takenaka Kazumasa <bzzs.1120@gmail.com>
2020-09-26 13:22:02 +0700

    remove unnecessary css

--- frontend/src/components/organisms/ActionMessage/style.ts
+++ frontend/src/components/organisms/ActionMessage/style.ts
@@ -1,16 +1,14 @@
 import styled from "@emotion/styled";
 
 export const ActionMessageWrapper = styled.div`
-  background: rgba(59, 125, 233, 0.9);
   font-size: 13px;
   line-height: 50px;
-  padding-left: 15px;
   position: relative;
   text-align: center;
   &.color-success {
-    background: rgba(59, 125, 233, 0.9);
+    background: blue;
   }
   &.color-failure {
-    background: rgb(245, 117, 117, 0.9);
+    background: pink;
   }
 `;
--- frontend/src/components/organisms/MainHeader/styles.ts
+++ frontend/src/components/organisms/MainHeader/styles.ts
@@ -2,34 +2,16 @@ import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
   display: flex;
-  flex-flow: row nowrap;
-  padding: 8px 0px 12px 20px;
-  color: #3a3a3a;
+  padding: 8px;
   justify-content: space-between;
-  height: 40px;
-  border-bottom: 1px solid #d4d8dd;
-  background-color: #ffffff;
-  svg {
-    color: #7c8291;
-  }
 `;
 
 export const Divide = styled.div`
-  height: 18px;
-  border-left: 1px solid #d8d8d8;
-  margin: 0 11px 0 18px;
+  border-left: 1px solid black;
 `;
 
-export const LeftContent = styled.div`
-  display: flex;
-  box-sizing: content-box;
-  padding-top: 2px;
-`;
+export const LeftContent = styled.div``;
 
-export const RightContent = styled.div`
-  display: flex;
-`;
+export const RightContent = styled.div``;
 
-export const Item = styled.div`
-  margin-right: 32px;
-`;
+export const Item = styled.div``;
