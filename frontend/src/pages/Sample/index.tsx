10ae656a07da71f308b554a91484bd0dda2310fa
Takenaka Kazumasa <bzzs.1120@gmail.com>
2020-10-11 00:32:01 +0700

    enable csv download (need refactor)

--- frontend/src/pages/Sample/index.tsx
+++ frontend/src/pages/Sample/index.tsx
@@ -9,6 +9,12 @@ import {
   typeGetSampleRequest,
 } from "store/actionCreators/sample";
 import { TypeSample } from "constants/type";
+import { Radio } from "antd";
+
+const charsets = [
+  { label: "UTF-8", value: "utf8" },
+  { label: "Shift_JIS", value: "sjis" },
+];
 
 const SamplePage = ({
   samples,
@@ -17,6 +23,7 @@ const SamplePage = ({
 }: Props) => {
   useEffect(getSamplesRequest, []);
   const [sample, setSample] = useState<any>(null);
+  const [charset, setCharset] = useState<"utf8" | "sjis">("utf8");
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setSample({ ...sample, [e.target.name]: e.target.value });
@@ -31,11 +38,29 @@ const SamplePage = ({
     }
   };
 
+  const onCharsetSelected = (e: any) => {
+    setCharset(e.target.value);
+  };
+
   return (
     <MainTemplate>
       <h1>Sample</h1>
       <h2>List</h2>
       <SampleList samples={samples} />
+
+      <Radio.Group onChange={onCharsetSelected} value={charset}>
+        {charsets.map((item, i) => (
+          <Radio key={i} value={item.value}>
+            {item.label}
+          </Radio>
+        ))}
+      </Radio.Group>
+      <a
+        href={`http://127.0.0.1:8080/api/v1/export/samples?charset=${charset}`}
+        download
+      >
+        csv
+      </a>
 
       <h2>New Sample</h2>
       <SampleForm onChange={onChange} onSubmit={onSubmit} />
