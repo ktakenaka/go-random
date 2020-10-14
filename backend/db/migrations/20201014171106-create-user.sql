968a0c8d02b7e63738c708f15e89abda9687f445
Takenaka Kazumasa <bzzs.1120@gmail.com>
2020-10-15 00:21:28 +0700

    re-create migration files

--- backend/db/migrations/20201014171106-create-user.sql
+++ backend/db/migrations/20201014171106-create-user.sql
@@ -1,9 +1,14 @@
+
+-- +migrate Up
 CREATE TABLE IF NOT EXISTS users(
   id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  email VARCHAR(255) NOT NULL,
+  email VARCHAR(100) NOT NULL,
   google_sub VARCHAR(30) NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   INDEX google_sub_index(google_sub),
   UNIQUE KEY unique_google_sub(google_sub)
 );
+
+-- +migrate Down
+DROP TABLE IF EXISTS users;
