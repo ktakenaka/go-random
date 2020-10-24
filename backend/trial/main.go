f655fba8e935c1889b6d3bf46db37ead4e356371
Takenaka Kazumasa <bzzs.1120@gmail.com>
2020-10-24 15:21:23 +0700

    try linked list

--- backend/trial/main.go
+++ backend/trial/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/list"
 	"fmt"
 	"reflect"
 	"time"
@@ -22,18 +23,6 @@ type Sample struct {
 	Content null.String `validate:"max=100" csv:"コンテント"`
 	UserID  uint64      `validate:"required" csv:"ユーザーID"`
 	Hoge    *int        `csv:"fuga"`
-}
-
-func main() {
-	hoge := 1
-	s := reflect.ValueOf(Sample{
-		Model:  Model{ID: 1, CreatedAt: time.Now(), UpdatedAt: time.Now()},
-		Title:  "title",
-		UserID: 1,
-		Hoge:   &hoge,
-	})
-	res := make(map[string]interface{})
-	iterateFields(s, res)
 }
 
 // Tag => Lookup, Get
@@ -61,4 +50,47 @@ func iterateFields(val reflect.Value, res map[string]interface{}) {
 		}
 	}
 	fmt.Println(res)
+}
+
+// NewListElm constructor for list Element
+func NewListElm(v interface{}) *list.Element {
+	return &list.Element{Value: v}
+}
+
+func main() {
+	hoge := 1
+	s := reflect.ValueOf(Sample{
+		Model:  Model{ID: 1, CreatedAt: time.Now(), UpdatedAt: time.Now()},
+		Title:  "title",
+		UserID: 1,
+		Hoge:   &hoge,
+	})
+	res := make(map[string]interface{})
+	iterateFields(s, res)
+
+	fmt.Println("===== linked list =====")
+	lis := list.New()
+	el1 := NewListElm("el1")
+	el2 := NewListElm("el2")
+	el3 := NewListElm("el3")
+	lis.PushBack(el1)
+	lis.PushBack(el2)
+	lis.InsertBefore("Hello", el1)
+	lis.PushFront(el3)
+
+	lis.MoveAfter(el2, el3)
+
+	var e *list.Element
+	e = lis.Front()
+	for i := 0; i < lis.Len(); i++ {
+		fmt.Printf("iteration %v\n", i)
+		fmt.Println(fmt.Sprintf("%v", e.Value))
+		e = e.Next()
+	}
+
+	fmt.Println("after refactoring")
+	var el *list.Element
+	for el = lis.Front(); el != nil; el = el.Next() {
+		fmt.Println(el.Value)
+	}
 }
