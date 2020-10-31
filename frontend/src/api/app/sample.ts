5846487e5fcd980eed97e5f653b8a6a785812bb9
Kazumasa Takenaka <bzzs.1120@gmail.com>
2020-11-01 01:18:03 +0700

    memo for be query test

--- frontend/src/api/app/sample.ts
+++ frontend/src/api/app/sample.ts
@@ -5,6 +5,7 @@ import { TypeSample } from "constants/type";
 export const list = (): Promise<AxiosResponse<any>> => {
   return API.get("/samples");
 };
+// samples?sort=title,-content&filter[title]=title&filter[content]=content&page[limit]=20&page[offset]=1
 
 export const get = (id: number): Promise<AxiosResponse<any>> => {
   return API.get(`/samples/${id}`);
