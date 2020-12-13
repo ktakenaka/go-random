3a8e4af64ec8671de013a2e90ddee5329d1a0b74
Kazumasa Takenaka <bzzs.1120@gmail.com>
2020-12-13 16:12:57 +0700

    fix migrations

--- backend/db/migrations/20201014171210-create-sample.sql
+++ backend/db/migrations/20201014171210-create-sample.sql
@@ -1,13 +1,14 @@
 
 -- +migrate Up
 CREATE TABLE IF NOT EXISTS `samples`(
-  `id` INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  `id` VARCHAR(32) PRIMARY KEY,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `user_id` INT UNSIGNED NOT NULL,
+  `user_id` VARCHAR(32) NOT NULL,
   `title` VARCHAR(20) NOT NULL,
   `content` VARCHAR(100) NULL,
 
+  FULLTEXT INDEX `ft_samples`(`title`, `content`) WITH PARSER ngram,
   CONSTRAINT `fk_samples_user_id` FOREIGN KEY(`user_id`) REFERENCES `users`(`id`),
   CONSTRAINT `uk_samples_user_id_title` UNIQUE(`user_id`, `title`)
 );
