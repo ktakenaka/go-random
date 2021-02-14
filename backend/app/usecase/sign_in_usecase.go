69e698c5178ad8a8d01bd7f1bc0994464db25cee
ktakenaka <bzzs.1120@gmail.com>
2021-02-14 15:37:42 +0700

    integrate AppError

--- backend/app/usecase/sign_in_usecase.go
+++ backend/app/usecase/sign_in_usecase.go
@@ -5,14 +5,18 @@ import (
 	"time"
 
 	"github.com/ktakenaka/go-random/backend/app/domain/repository"
+	appErr "github.com/ktakenaka/go-random/backend/app/errors"
 	"github.com/ktakenaka/go-random/backend/pkg/jwtutil"
+	"golang.org/x/xerrors"
 )
 
+// SignInUsecase usecase
 type SignInUsecase struct {
 	googleRepo repository.GoogleRepository
 	userRepo   repository.UserRepository
 }
 
+// NewSignInUsecase constructor
 func NewSignInUsecase(
 	gRepo repository.GoogleRepository, uRepo repository.UserRepository,
 ) *SignInUsecase {
@@ -22,28 +26,33 @@ func NewSignInUsecase(
 	}
 }
 
+// Execute sign in flow
 func (uc *SignInUsecase) Execute(code, nonce string) (aTkn, rTkn, csrfTkn string, err error) {
 	ctx := context.Background()
 
 	token, err := uc.googleRepo.GetToken(ctx, code, nonce)
 	if err != nil {
-		return "", "", "", err
+		err = appErr.NewAppError(err)
+		return "", "", "", xerrors.Errorf("%w", err)
 	}
 
 	body, err := uc.googleRepo.GetUserInfo(ctx, token)
 	if err != nil {
-		return "", "", "", err
+		err = appErr.NewAppError(err)
+		return "", "", "", xerrors.Errorf("%w", err)
 	}
 
 	// TODO: put User entity
 	user, err := uc.userRepo.UpdateOrCreate(body)
 	if err != nil {
-		return "", "", "", err
+		err = appErr.NewAppError(err)
+		return "", "", "", xerrors.Errorf("%w", err)
 	}
 
 	csrfTkn, err = jwtutil.GenerateCSRFToken()
 	if err != nil {
-		return "", "", "", err
+		err = appErr.NewAppError(err)
+		return "", "", "", xerrors.Errorf("%w", err)
 	}
 
 	claims := jwtutil.AuthClaims{
@@ -53,5 +62,9 @@ func (uc *SignInUsecase) Execute(code, nonce string) (aTkn, rTkn, csrfTkn string
 	}
 
 	aTkn, rTkn, err = jwtutil.GenerateToken(&claims)
+	if err != nil {
+		err = appErr.NewAppError(err)
+		return "", "", "", xerrors.Errorf("%w", err)
+	}
 	return aTkn, rTkn, csrfTkn, err
 }
